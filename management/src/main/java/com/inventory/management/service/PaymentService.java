e694c4aa17d5d941b201e8580d69e70ad40af7b7
Shubh00796 <shubhamdongare9696@gmail.com>
2025-02-06 17:19:25 +0530

    Added the minor changes

--- management/src/main/java/com/inventory/management/service/PaymentService.java
+++ management/src/main/java/com/inventory/management/service/PaymentService.java
@@ -2,8 +2,13 @@ package com.inventory.management.service;
 
 import com.inventory.management.Dtos.PaymentRequestDTO;
 import com.inventory.management.Dtos.PaymentResponseDTO;
+import com.inventory.management.Model.Payment;
+
+import java.util.List;
 
 public interface PaymentService {
     PaymentResponseDTO processPayment(PaymentRequestDTO request);
+    List<Payment> getPaymentsByStripeId(String chargeId);
+    
 
 }
