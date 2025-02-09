c98a12bcc6fffab6c8e6fb3f1b14b5c3fe7cfed5
Shubh00796 <shubhamdongare9696@gmail.com>
2025-02-09 22:27:15 +0530

    Resolved merge conflicts

--- management/src/main/java/com/inventory/management/Factories/ContentFactory.java
+++
@@ -1,7 +0,0 @@
-package com.inventory.management.Factories;
-
-import com.inventory.management.Model.Content;
-
-public interface ContentFactory {
-    Content createContent(String id, String title, String description);
-}
