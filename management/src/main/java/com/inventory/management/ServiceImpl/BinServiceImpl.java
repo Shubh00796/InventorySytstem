ce835429cfa1667b9a82fd54fa2b61173d7266d3
Shubh00796 <shubhamdongare9696@gmail.com>
2025-02-22 22:19:12 +0530

    Resolved the Workflow

---
+++ management/src/main/java/com/inventory/management/ServiceImpl/BinServiceImpl.java
@@ -0,0 +1,45 @@
+package com.inventory.management.ServiceImpl;
+
+import com.inventory.management.Dtos.BinDTO;
+import com.inventory.management.Mapper.BinMapper;
+import com.inventory.management.ReposiotryServices.BinRepositryService;
+import com.inventory.management.service.BinService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BinServiceImpl implements BinService {
+    private final BinRepositryService repositryService;
+    private final BinMapper mapper;
+
+    @Override
+    public BinDTO createBin(BinDTO binDTO) {
+        return null;
+    }
+
+    @Override
+    public BinDTO updateBin(BinDTO binDTO) {
+        return null;
+    }
+
+    @Override
+    public Optional<BinDTO> getBinById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<BinDTO> getAllBins() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteBin(Long id) {
+
+    }
+}
